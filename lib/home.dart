84fca4aac42761b830e4d5aa5d2bbfe891e63350
anshd258 <anshd258@gmail.com>
2022-10-08 01:15:53 +0530

    added box to screen and sado in it and made te code clean

--- lib/home.dart
+++ lib/home.dart
@@ -11,39 +11,60 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.bottomCenter,
-        margin: EdgeInsets.only(bottom: 30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "/",
-                style: TextStyle(color: Colors.white),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                    const Color.fromARGB(255, 39, 39, 40)),
-              ),
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              width: 500,
+              height: 500,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  color: Color.fromARGB(255, 255, 99, 99),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      color: Color.fromARGB(105, 89, 89, 89),
+                      offset: Offset(7.5, 7.5),
+                      spreadRadius: 0,
+                    ),
+                  ]),
             ),
-            const SizedBox(
-              width: 100,
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            margin: EdgeInsets.only(bottom: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "<-",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 39, 39, 40)),
+                  ),
+                ),
+                const SizedBox(
+                  width: 100,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "->",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 39, 39, 40)),
+                  ),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "\\",
-                style: TextStyle(color: Colors.white),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                    const Color.fromARGB(255, 39, 39, 40)),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
